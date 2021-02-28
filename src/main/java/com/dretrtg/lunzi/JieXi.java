23f4e3e055c3bc36c367fb0a05d67e779cdcf98c
dretrtg <1367396040@qq.com>
2021-02-28 10:51:21 +0800

    '确定目标'

---
+++ src/main/java/com/dretrtg/lunzi/JieXi.java
@@ -0,0 +1,17 @@
+package com.dretrtg.lunzi;
+
+/**
+ * Created by 13673 on 2021/2/28.
+ * 实现一个功能，能够解析多重不规则的json字段嵌套
+ * {"category":{"category1":["pingguo", "huasheng"], "category2":["pijiu","baijiu"], "category3":{"category3.1":{"category3.2":["shangyi","xiayi"], "category3.3":["maoyi","maoku"]}},"category4":["123",null]}}
+ */
+
+import java.util.*;
+public class JieXi {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        Gson gson = new Gson();
+
+    }
+}
